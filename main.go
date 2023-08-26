e3d5518e1f0bd91b6d60b8d5138e87872037bede
samurai <samurai@avan.local>
2023-08-26 13:39:30 +0000

    handle create super user

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"core_api/api"
+	"core_api/database"
 	"core_api/utils"
 	"log"
-
+	"os"
 )
 
 func main() {
@@ -12,7 +13,20 @@ func main() {
 	conf := utils.NewConfig()
 	conf.LoadConfig(".")
 
-	// store := database.NewCrudHandler()
+	store := database.NewDBHandler()
+	if err := store.DBConnection(); err != nil {
+		log.Fatal("connention db error")
+	}
+
+	for _, arg := range os.Args[1:] {
+		if arg == "createsuperuser" {
+			db := database.NewUser()
+			if err := db.CreateSuperUser(); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
 	server := api.NewServer(conf.BIND_HOST, conf.BIND_PORT)
 	log.Fatal(server.Start())
 
