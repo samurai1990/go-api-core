96301079837f0e74eb15f6282ae3682d7613a1e0
samurai <samurai@avan.local>
2023-09-05 07:41:34 +0000

    change value database error

--- errors/errors.go
+++ errors/errors.go
@@ -3,7 +3,6 @@ package errors
 import "errors"
 
 var ErrBadInput = errors.New("bad input")
-var ErrConnectionDB = errors.New("failed to connect database")
-var ErrMigrationDB = errors.New("failed migrate schema")
+var ErrDB = errors.New("error database")
 var ErrEnvironmentValue = errors.New("not set environment variable")
 var ErrInternal = errors.New("internal error")
