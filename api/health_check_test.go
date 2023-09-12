036594873e1630d5daa4f7b9544247ca8e9067e3
samurai <samurai@avan.local>
2023-09-12 11:55:24 +0000

    Add: test health api

---
+++ api/health_check_test.go
@@ -0,0 +1,36 @@
+package api_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"core_api/api"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthRoute(t *testing.T) {
+	serv := api.NewServer("127.0.0.1", 80)
+	router := serv.Setup()
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/health", nil)
+	router.ServeHTTP(w, req)
+	res := SwapResponseToMapStruct(w.Body.Bytes())
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, true, CheckBodyOkErrors(res))
+}
+
+func makeRequestTest() map[string]interface{} {
+	mockRequest := strings.NewReader(`{"Username": "test", "Password": "test", "is_admin": 1,"email":"test@co.com"}`)
+	jsonValue, _ := json.Marshal(mockRequest)
+	serv := api.NewServer("127.0.0.1", 80)
+	router := serv.Setup()
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/health", bytes.NewBuffer(jsonValue))
+	router.ServeHTTP(w, req)
+	return SwapResponseToMapStruct(w.Body.Bytes())
+}
